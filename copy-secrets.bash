e80643c72bf3ee391ae3ea468b14df14172a37e3
Peter van Zetten <peter.van.zetten@cgi.com>
2019-04-17 14:14:25 +0100

    Fix copying secrets with ca.crt=null

--- copy-secrets.bash
+++ copy-secrets.bash
@@ -5,14 +5,14 @@ set -x
 SOURCE_NAMESPACE_LABEL=copy-secrets-source-namespace
 SOURCE_NAME_LABEL=copy-secrets-source-name
 
-for namespace in $(kubectl get namespaces); do
+for namespace in $(kubectl get namespaces --no-headers -o custom-columns=':metadata.name'); do
     # Filter by those secrets which already have our labels
-    for name in $(kubectl get secrets --namespace=${namespace} --selector copy-secrets-source-name --output=json | jq -r '.items[].metadata.name'); do
+    for name in $(kubectl get secrets --namespace=${namespace} --selector copy-secrets-source-name --no-headers -o custom-columns=':metadata.name'); do
         # Extract the source secret name and namespace from this secret's labels
         JSON=$(kubectl get secrets --namespace=${namespace} ${name} --output=json)
         SOURCE_SECRET_NAMESPACE=$(echo $JSON | jq -r .metadata.labels.\"${SOURCE_NAMESPACE_LABEL}\")
         SOURCE_SECRET_NAME=$(echo $JSON | jq -r .metadata.labels.\"${SOURCE_NAME_LABEL}\")
         # Copy over the source secret
-        kubectl get secret --namespace=${SOURCE_SECRET_NAMESPACE} ${SOURCE_SECRET_NAME} --export --output=json | jq  ".metadata.name = \"${name}\"" | kubectl apply --namespace=${namespace} -f - || true
+        kubectl get secret --namespace=${SOURCE_SECRET_NAMESPACE} ${SOURCE_SECRET_NAME} --output=json | jq "del(.metadata.creationTimestamp, .metadata.namespace, .metadata.resourceVersion, .metadata.uid) | .metadata.name = \"${name}\" | del(.data[\"ca.crt\"] | select(. == null))" | kubectl apply --namespace=${namespace} -f - || true
     done
 done
